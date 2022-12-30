7d1b8bea30f1987180aeee4b69c59709495a30f6
Diego Mora <diegomorademc@gmail.com>
2022-12-29 23:15:52 -0500

    Ajuste sequelize

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.NAME || 'almacen', process.env.USER || 'root', process.env.DB_PASSWORD || 'admin12345', {
+const sequelize = new Sequelize(process.env.DB_NAME || 'almacen', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'admin12345', {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql'
   });
