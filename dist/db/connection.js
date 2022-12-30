f06ff72aec96f29dc9a044503b324fdc347bb862
Diego Mora <diegomorademc@gmail.com>
2022-12-30 00:22:55 -0500

    Ajuste archivo connection

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize(process.env.NAME || 'almacen', process.env.USER || 'root', process.env.DB_PASSWORD || 'admin12345', {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql'
+const sequelize = new sequelize_1.Sequelize('almacen', 'root', 'admin12345', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 8889
 });
 exports.default = sequelize;
