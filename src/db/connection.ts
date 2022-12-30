0f6951efedc5e4e86db44bb7408d25a13a6bec8b
Diego Mora <diegomorademc@gmail.com>
2022-12-29 23:49:34 -0500

    A

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize('almacen', 'root', 'admin12345', {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
-    port: 8889
+    port: 3306
   });
 
   export default sequelize;
