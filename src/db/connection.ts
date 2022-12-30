cb5174cbc80608dcddaf4dbdd144505d7755bca1
Diego Mora <diegomorademc@gmail.com>
2022-12-30 01:34:49 -0500

    Ajuste4 connection.js

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DB_NAME || 'almacen', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'admin12345', {
     host: process.env.DB_HOST || 'localhost',
+    port: 3306,    
     dialect: 'mysql'
   });
 
