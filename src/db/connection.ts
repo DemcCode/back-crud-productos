8120c86c8689d1b144f8deb06c8b1e6dd033a385
Diego Mora <diegomorademc@gmail.com>
2022-12-29 23:47:40 -0500

    Ajuste sequelize

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('almacen', 'root', 'admin12345', {
-    host: 'localhost',
-    dialect: 'mysql'
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    port: 8889
   });
 
   export default sequelize;
