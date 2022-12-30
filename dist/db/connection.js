0eb0b53c013a9fe1c365de852f35491944c1e84b
Diego Mora <diegomorademc@gmail.com>
2022-12-30 10:04:50 -0500

    Ajuste6 connection.js

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME || 'almacen', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'admin12345', {
     dialect: 'mysql',
-    port: 7283,
+    port: 7178,
     host: process.env.DB_HOST || 'localhost',
 });
 exports.default = sequelize;
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DB_NAME || 'almacen', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'admin12345', {
   dialect: 'mysql',
-  port: 7283,
+  port: 7178,
   host: process.env.DB_HOST || 'localhost',
   });
 
